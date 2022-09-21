7ea29fa65d3e7e005a2636837709e38153478fb6
Jeremiah <j.a.carlson.93@gmail.com>
2022-09-21 00:59:18 -0400

    add redirects and borders

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const mainTitle = document.querySelector("div#header h1");
-
-console.log(mainTitle.innerHTML);
+function redirect(loc) {
+  if (loc == "senneca") {
+    location.href = "https://hmf-dev.github.io/Style-Guide/senneca/testing/";
+  } else if (loc == "hmf") {
+    location.href = "https://hmf-dev.github.io/Style-Guide/hmf/testing/";
+  } else {
+    console.log("Selected page does not exist.");
+  }
+}
